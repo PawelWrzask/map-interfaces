29e702686918e0b440c3a4131dab72895179afbf
Paweł Wrzask <wrzask.pawel@gmail.com>
2018-06-12 14:13:28 +0200

    displaying images

--- app/src/main/java/jm/maps/view/FragmentDiscover.java
+++ app/src/main/java/jm/maps/view/FragmentDiscover.java
@@ -3,12 +3,17 @@ package jm.maps.view;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,68 +35,134 @@ import jm.maps.R;
 public class FragmentDiscover extends Fragment {
 
     Context context;
-    private LinearLayout mainTable;
+    private TableLayout mainTable;
     List<Integer> animalsList;
 
-    static final String[] EXTENSIONS = new String[]{
-            "jpg", "png", "bmp"
-    };
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
 
         mainTable = view.findViewById(R.id.TableRow02);
         context = mainTable.getContext();
 
         loadAnimals();
+        Resources resources = context.getResources();
+
+        int rows = 6;
+        int columns = 6;
+        for(int i = 0 ; i < rows ; i++){
+            TableRow tableRow = new TableRow(getActivity());
+            for(int j = 0 ; j < columns ; j++){
+                ImageView newImage = new ImageView(getActivity());
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                options.inSampleSize = 3;
+
+                Bitmap originalBmp = BitmapFactory.decodeResource(context.getResources(), animalsList.get(i*columns+j));
+                int imageSize = originalBmp.getWidth() > originalBmp.getHeight() ? originalBmp.getHeight() : originalBmp.getWidth();
+                Bitmap croppedBmp = Bitmap.createBitmap(originalBmp, 0, 0, imageSize, imageSize);
 
 
-
-        for(int i = 0 ; i < 6 ; i++){
-            ImageView newImage = new ImageView(getActivity());
-
-            Bitmap originalBmp = BitmapFactory.decodeResource(context.getResources(), animalsList.get(0));
-            int imageSize = originalBmp.getWidth() > originalBmp.getWidth() ? originalBmp.getWidth() : originalBmp.getWidth();
-            Bitmap croppedBmp = Bitmap.createBitmap(originalBmp, 0, 0, imageSize, imageSize);
-
-
-            newImage.setImageBitmap(croppedBmp);
-            newImage.setMaxHeight(250);
-            newImage.setMaxWidth(250);
-            newImage.layout(150,150,150,150);
-            newImage.setAdjustViewBounds(true);
-            mainTable.addView(newImage);
+                newImage.setImageBitmap(croppedBmp);
+                newImage.setMaxHeight(250);
+                newImage.setMaxWidth(250);
+                newImage.setAdjustViewBounds(true);
+                tableRow.addView(newImage);
+            }
+            mainTable.addView(tableRow);
         }
 
 
         return view;
     }
 
-    private void loadAnimals() {
-        File dir = new File("res");
-        File[] filelist = dir.listFiles(IMAGE_FILTER);
-        animalsList = new ArrayList();
+    private void loadAnimals(){
+        animalsList = new ArrayList<>();
 
-        for (File f : filelist) { // do your stuff here }
-            //animalsList.add(Drawable.createFromPath(f.getPath()));
-        }
+        animalsList.add(R.drawable.barczatka_kataks);
+        animalsList.add(R.drawable.bocian_bialy);
+        animalsList.add(R.drawable.bogatek_wspanialy);
+        animalsList.add(R.drawable.czerwonczyk_fioletek);
+        animalsList.add(R.drawable.dymowka);
+        animalsList.add(R.drawable.dzierzba_rudoglowa);
+        animalsList.add(R.drawable.foka_pospolita);
+        animalsList.add(R.drawable.gagol);
+        animalsList.add(R.drawable.ggniewosz_plamisty);
+        animalsList.add(R.drawable.gluszec_zwyczajny);
+        animalsList.add(R.drawable.gorowka_sudecka);
+        animalsList.add(R.drawable.iglica_mala);
+        animalsList.add(R.drawable.iglicznia);
+        animalsList.add(R.drawable.jaszczurka_zielona);
+        animalsList.add(R.drawable.karliczka_zwyczajna);
+        animalsList.add(R.drawable.koszatka_lesna);
+        animalsList.add(R.drawable.kozica_polnocna);
+        animalsList.add(R.drawable.krakwa);
+        animalsList.add(R.drawable.kraska_zwyczajna);
+        animalsList.add(R.drawable.krasopani_hera);
+        animalsList.add(R.drawable.kreslinek_nizinny);
+        animalsList.add(R.drawable.kukulka_czubata);
+        animalsList.add(R.drawable.modliszka_zwyczajna);
+        animalsList.add(R.drawable.modraszek_arion);
+        animalsList.add(R.drawable.modraszek_eros);
+        animalsList.add(R.drawable.modraszek_gniady);
+        animalsList.add(R.drawable.modraszek_nausitous);
+        animalsList.add(R.drawable.modraszek_telejus);
+        animalsList.add(R.drawable.nerpa_obraczkowana);
+        animalsList.add(R.drawable.niedzwiedz_brunatny);
+        animalsList.add(R.drawable.niepylak_apollo);
+        animalsList.add(R.drawable.niepylak_mnemozyna);
+        animalsList.add(R.drawable.nocek_brandta);
+        animalsList.add(R.drawable.nocek_duzy);
+        animalsList.add(R.drawable.norka_europejska);
+        animalsList.add(R.drawable.nur_czarnoszyi);
+        animalsList.add(R.drawable.nurzyk_polarny);
+        animalsList.add(R.drawable.plaskonos_zwyczajny);
+        animalsList.add(R.drawable.pliszka_siwa);
+        animalsList.add(R.drawable.plywak_szerokobrzezek);
+        animalsList.add(R.drawable.podkowiec_duzy);
+        animalsList.add(R.drawable.przeplatka_aurinia);
+        animalsList.add(R.drawable.przeplatka_maturna);
+        animalsList.add(R.drawable.rys_euroazjatycki);
+        animalsList.add(R.drawable.rzekotka_drzewna);
+        animalsList.add(R.drawable.sep_plowy);
+        animalsList.add(R.drawable.sikora_lazurowa);
+        animalsList.add(R.drawable.slepowron_zwyczajny);
+        animalsList.add(R.drawable.smuzka_lesna);
+        animalsList.add(R.drawable.smuzka_stepowa);
+        animalsList.add(R.drawable.strzepotek_edypus);
+        animalsList.add(R.drawable.strzepotek_hero);
+        animalsList.add(R.drawable.susel_moregowany);
+        animalsList.add(R.drawable.susel_perelkowany);
+        animalsList.add(R.drawable.swistak_tatrzanski);
+        animalsList.add(R.drawable.swistun_zwyczajny);
+        animalsList.add(R.drawable.swistun_zwyczajny);
+        animalsList.add(R.drawable.szarytka_morska);
+        animalsList.add(R.drawable.tecznik_ziarenkowaty);
+        animalsList.add(R.drawable.traszka_grzebieniasta);
+        animalsList.add(R.drawable.traszka_karpacka);
+        animalsList.add(R.drawable.trzepla_zielona);
+        animalsList.add(R.drawable.warzecha_zwyczajna);
+        animalsList.add(R.drawable.wasatka);
+        animalsList.add(R.drawable.wilk_szary);
+        animalsList.add(R.drawable.wwaz_eskulapa);
+        animalsList.add(R.drawable.zaba_moczarowa);
+        animalsList.add(R.drawable.zadrzechnia_czarnoroga);
+        animalsList.add(R.drawable.zagnica_zielona);
+        animalsList.add(R.drawable.zajac_bielak);
+        animalsList.add(R.drawable.zalotka_bialoczelna);
+        animalsList.add(R.drawable.zbik_europejski);
+        animalsList.add(R.drawable.zimorodek_zwyczajny);
+        animalsList.add(R.drawable.zolednica_europejska);
+        animalsList.add(R.drawable.zolna_zwyczajna);
+        animalsList.add(R.drawable.zolw_blotny);
+        animalsList.add(R.drawable.zubr_europejski);
     }
 
-        static final FilenameFilter IMAGE_FILTER = new FilenameFilter() {
 
-            @Override
-            public boolean accept(final File dir, final String name) {
-                for (final String ext : EXTENSIONS) {
-                    if (name.endsWith("." + ext)) {
-                        return (true);
-                    }
-                }
-                return (false);
-            }
-        };
 
 
     private TableRow createRow(int y) {
